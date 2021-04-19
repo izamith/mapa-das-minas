7768045bb5d318b25724a1ee86d8ffd7bf040015
Isabela Zamith <izamith@yahoo.com.br>
2021-04-19 00:31:34 -0300

    Correção Bug no box-info

--- scripts/map.js
+++ scripts/map.js
@@ -55,7 +55,7 @@ map.on('load', function () {
     addSource()
     addLayers()
     
-    //bolinhas em clusters :)
+    //clusters
     map.on('click',/* cluster layer id */ 'clusters', function (e) {
       e.preventDefault();
       var features = map.queryRenderedFeatures(e.point, { layers: ['clusters'] });
@@ -105,8 +105,23 @@ map.on('load', function () {
        
 
 
-    });
-    
+      });
+        
+        map.on('mouseenter', 'clusters', function () {
+        map.getCanvas().style.cursor = 'pointer';
+        });
+        map.on('mouseleave', 'clusters', function () {
+        map.getCanvas().style.cursor = '';
+        });
+
+        map.on('click', function(e) {
+          if (e.defaultPrevented === false) {
+            console.log('hide taskbar');
+            document.getElementById('box-info').style.display="none"
+          }
+        });
+
+
     //bolinhas sozinhas :(
     map.on('click', 'unclustered-point', function (e) {
       var perfilEmInfo = e.features[0].properties.mag;
@@ -127,17 +142,45 @@ map.on('load', function () {
         caixa.appendChild(a);
     })
 
+
+    /*  
+    // When a click event occurs on a feature in
+    // the unclustered-point layer, open a popup at
+    // the location of the feature, with
+    // description HTML from its properties.
+    map.on('click', 'unclustered-point', function (e) {
+    var coordinates = e.features[0].geometry.coordinates.slice();
+    var mag = e.features[0].properties.mag;
+    var tsunami;
+     
+    if (e.features[0].properties.tsunami === 1) {
+    tsunami = 'yes';
+    } else {
+    tsunami = 'no';
+    }
+     
+    // Ensure that if the map is zoomed out such that
+    // multiple copies of the feature are visible, the
+    // popup appears over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+     
+    new mapboxgl.Popup()
+    .setLngLat(coordinates)
+    .setHTML(
+    'magnitude: ' + mag + '<br>Was there a tsunami?: ' + tsunami
+    )
+    .addTo(map);
+    });
+     
     map.on('mouseenter', 'clusters', function () {
-      map.getCanvas().style.cursor = 'pointer';
+    map.getCanvas().style.cursor = 'pointer';
     });
     map.on('mouseleave', 'clusters', function () {
-      map.getCanvas().style.cursor = '';
+    map.getCanvas().style.cursor = '';
     });
-    map.on('click', function(e) {
-      if (e.defaultPrevented === false) {
-        console.log('hide taskbar');
-        document.getElementById('box-info').style.display="none"
-      }
-    });
-    
+  */
 });
+
+//////////////////////////////HELPER FUNCTIONS//////////////////////////////////

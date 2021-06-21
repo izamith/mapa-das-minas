5ddf1b58c774f42eebec74269f5ce0899b075731
Isabela Zamith <izamith@yahoo.com.br>
2021-06-21 15:28:36 -0300

    Adc minas 21. jun

--- scripts/helperFunctions.js
+++ scripts/helperFunctions.js
@@ -449,8 +449,8 @@ function addSource() {
              { "type": "Feature", "properties": { "mag": pessoasConjunto[204], "lugar": lugaresConjunto[204], "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": coordenadasConjunto[204] } },
              { "type": "Feature", "properties": { "mag": pessoasConjunto[205], "lugar": lugaresConjunto[205], "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": coordenadasConjunto[205] } },
              { "type": "Feature", "properties": { "mag": pessoasConjunto[206], "lugar": lugaresConjunto[206], "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": coordenadasConjunto[206] } },
-             /*
              { "type": "Feature", "properties": { "mag": pessoasConjunto[207], "lugar": lugaresConjunto[207], "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": coordenadasConjunto[207] } },
+             /*
              { "type": "Feature", "properties": { "mag": pessoasConjunto[208], "lugar": lugaresConjunto[208], "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": coordenadasConjunto[208] } },
              { "type": "Feature", "properties": { "mag": pessoasConjunto[209], "lugar": lugaresConjunto[209], "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": coordenadasConjunto[209] } },
              { "type": "Feature", "properties": { "mag": pessoasConjunto[210], "lugar": lugaresConjunto[210], "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": coordenadasConjunto[210] } },
